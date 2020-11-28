49d556a3db24457f74a2871c3dad3bdb769f5d5a
Anwesh43 <anweshthecool0@gmail.com>
2020-11-29 03:50:57 +0530

    adding Renderer class

--- linerotexpandarcview/src/main/java/com/example/linerotexpandarcview/LineRotExpandArcView.kt
+++ linerotexpandarcview/src/main/java/com/example/linerotexpandarcview/LineRotExpandArcView.kt
@@ -185,4 +185,27 @@ class LineRotExpandArcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotExpandArcView) {
+
+        private val animator : Animator = Animator(view)
+        private val lrea : LineRotExpandArc = LineRotExpandArc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrea.draw(canvas, paint)
+            animator.animate {
+                lrea.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrea.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
