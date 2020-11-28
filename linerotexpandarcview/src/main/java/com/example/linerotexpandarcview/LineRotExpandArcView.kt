9252e3af05176de29d3f84c37103f353c3c781f9
Anwesh43 <anweshthecool0@gmail.com>
2020-11-29 03:26:36 +0530

    adding LineRoitExpandArcView class

--- linerotexpandarcview/src/main/java/com/example/linerotexpandarcview/LineRotExpandArcView.kt
+++ linerotexpandarcview/src/main/java/com/example/linerotexpandarcview/LineRotExpandArcView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLREANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLREANode(i, scale, paint)
 }
+
+class LineRotExpandArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
